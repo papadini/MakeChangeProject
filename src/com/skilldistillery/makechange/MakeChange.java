bfb5a87c4614bef5f12acd0a51370f0010976e04
papadini <krpaladini@gmail.com>
2018-08-31 22:46:52 -0600

    Created register array

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -3,34 +3,43 @@ package com.skilldistillery.makechange;
 import java.util.Scanner;
 
 public class MakeChange {
-	// multiple price of item and users money by 100 the  turn back into change.
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		//The user is prompted asking for the price of the item.
 		System.out.println("Hello shopper, enter price of your item.");
-		System.out.print("Item Price: $");
-		double itemPrice = kb.nextDouble();
+		double price = itemPrice();
 		System.out.print("Next, enter dollar amount: $");
 		double userMoney = kb.nextDouble();
-//		double itemPrice = 9.99;
-//		double userMoney = 20.00;		// change = 2x $20, 1 x $10, 0 x $5, 2 x $1, 1 x 25c, 2x 10c,0x5c, 3x1c
+		notEnoughOrEqual(userMoney, price);
+		double change = (userMoney - price);
+
+		//Register register = new Register();
+		//register.checkMoney(change);
+		RegisterArry regArry = new RegisterArry ();
+		regArry.moneyArray(change);
+
+		kb.close();
+	}
+	public static void notEnoughOrEqual (double money, double price) {
 		
-		
-		if ( userMoney < itemPrice ) {
+		if (money < price) {
 			System.out.println("Sorry, that is not enough.");
 			return;
 		}
-		double change = Math.round((userMoney - itemPrice)*100.00)/100.00;
-		
-		Register register = new Register();
-		double newAmt = register.changeMoney(change);
-		register.checkMoney(newAmt);
-		
-		
-		kb.close();
-		
-
+		else if (money == price ) {
+			System.out.println("Wow exact change, you rock!");
+		}
 	}
+	public static double itemPrice ( ) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Item Price: $");
+		double itemPrice = keyboard.nextDouble();
+		
+		//keyboard.close();
+		
+		return itemPrice;
+		
+	}
+	
 
 }
--- src/com/skilldistillery/makechange/Register.java
+++ src/com/skilldistillery/makechange/Register.java
@@ -1,14 +1,8 @@
 package com.skilldistillery.makechange;
 
 public class Register {
-	double newAmt, currentChange;
+	double newAmt;
 
-	public double changeMoney(double money) {
-		newAmt = money;
-		System.out.println(newAmt);
-		return newAmt;
-
-	}
 
 	public void checkMoney(double amount) {
 		amount = newAmt;
@@ -49,24 +43,53 @@ public class Register {
 			System.out.println("dims: " + dimeCount); // amount of .10 in change
 			amount = amount - (dimeCount * .10);
 		}
-		if (newAmt >= .5) {
-			newAmt = (amount % .5);
-			int nickelCount = (int) ((amount - newAmt) / .5);
-			System.out.println("nickels: " + nickelCount); // amount of .5 in change
-			amount = amount - (nickelCount * .5);
-			System.out.println(amount);
+		if (newAmt >= .049) {
+			if (newAmt >= .05) {
+				newAmt = (amount % .05);
+				int nickelCount = (int) ((amount - newAmt) / .05);
+				System.out.println("nickels: " + nickelCount); // amount of .5 in change
+				amount = amount - (nickelCount * .05);
+			} else {
+				System.out.println("nickels: 1");
+				return;
+			}
 		}
-		if (newAmt >= .01) {
-			newAmt = (amount % .01);
-			int pennyCount = (int) ((amount - newAmt) / .01);
-			System.out.println("pennies: " + pennyCount); // amount of .01 in change
-
+		if (newAmt >= .009) {
+			if (newAmt >= .01) {
+				newAmt = (amount % .01);
+				int pennyCount = (int) ((amount - newAmt) / .01);
+				System.out.println("pennies: " + pennyCount); // amount of .01 in change
+			} else {
+				System.out.println("pennies: 1");
+			}
 		}
 
-	}
+	}	
+//	public void moneyArray(double amount) {
+//		 newAmt = amount;
+//		double[] moneyArr = { 20, 10, 5, 1, .25, .10, .05, .01 };
+//		String[] nameArr = { "Twenty: ", "Ten: ", "Fives: ", "Ones: ", "Quarters: ",
+//				"Dimes: ", "Nickels: ", "Pennies: " };
+//
+//		System.out.println("Your change is: ");
+//		for (int i = 0; i < moneyArr.length; i++) {
+//			amount = newAmt;
+//			if (newAmt >= moneyArr[i]) {
+//				newAmt = (amount % moneyArr[i]);
+//				int moneyCount = (int) ((amount - newAmt) / moneyArr[i]);
+//				System.out.println(nameArr[i] + moneyCount); // amount of 20s in change
+//				amount = amount - (moneyCount * moneyArr[i]);
+//			}
+//		}
+//		if (amount <= .5 && amount > .01) {
+//			System.out.println("Nickels: 1");
+//		}
+//		if (amount <= .01 && amount > .008) {
+//			System.out.println("Pennies: 1");
+//		}
+//
+//	}
 
 }
-	
-
 
 
